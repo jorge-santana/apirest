19a8f9ef6c21611da6d6083d0225173776b3069a
Jorge Sant Ana <jorge@argus-academy.com>
2025-02-14 07:35:16 -0300

    :tada: feat: Nest.js - Conceitos Básicos

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('uma string qualquer');
-
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
