5b729a3d1372670ae0c76402f90e489af3fcd4b0
Jorge Sant Ana <jorge@argus-academy.com>
2025-02-17 14:15:01 -0300

    :tada: feat: Módulo - Prática

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CustomerModule } from './customer/customer.module';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { RentalModule } from './rental/rental.module';
+import { PaymentModule } from './payment/payment.module';
+import { PdfModule } from './pdf/pdf.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    CustomerModule,
+    VehicleModule,
+    RentalModule,
+    PaymentModule,
+    PdfModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
