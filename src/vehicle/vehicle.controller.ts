99af83f62bf05bafbbcbecdc21caaaafe29f0413
Jorge Sant Ana <jorge@argus-academy.com>
2025-02-18 12:25:26 -0300

    :tada: feat: Métodos HTTP e os Decoradores de Métodos HTTP

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('api/v1/vehicle')
 export class VehicleController {
   @Get()
   findAll() {
+    // TODO: Retornar todos os registros de vículos
     return [{ brand: 'Toyota' }, { brand: 'Ford' }];
+  }
+
+  @Get(':id')
+  findOne() {
+    // TODO: Recuperar um veículo por id
+    return { message: 'Retorna um veículo por id' };
+  }
+
+  @Post()
+  save() {
+    // TODO: Registrar um veículo
+    return { message: 'Registrar veículo' };
+  }
+
+  @Patch(':id')
+  update() {
+    // TODO: Atualizar um veículo (parcial)
+    return { message: 'Atualizar parte de um veículo' };
+  }
+
+  @Put(':id')
+  replace() {
+    // TODO: Atualizar todos os dados de um veículo (substituir)
+    return { message: 'Atualiza todos os dados de um veículo' };
+  }
+
+  @Delete(':id')
+  delete() {
+    // TODO: Remover um veículo
+    return { message: 'Remove um veículo' };
   }
 }
