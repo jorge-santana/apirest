a04f96552733ecbc2c3ad9a1902afee4af4b6045
Jorge Sant Ana <jorge@argus-academy.com>
2025-03-20 10:04:54 -0300

    :tada: feat: Criando um Middleware e Aplicando a um Módulo

--- src/vehicle/dto/find-vehicle.dto.ts
+++ src/vehicle/dto/find-vehicle.dto.ts
@@ -1,5 +1,12 @@
+import { IsOptional } from 'class-validator';
+
 export class FindVehicleDto {
+  @IsOptional()
   brand: string;
+
+  @IsOptional()
   seats: number;
+
+  @IsOptional()
   uppercase: boolean;
 }
--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { VehicleController } from './vehicle.controller';
 import { MaintenenceController } from './maintenence/maintenence.controller';
 import { InspectionController } from './inspection/inspection.controller';
 import { VehicleService } from './vehicle.service';
+import { AuthorizationMiddleware } from 'src/middlewares/authorization/authorization.middleware';
 
 @Module({
   controllers: [VehicleController, MaintenenceController, InspectionController],
   providers: [VehicleService],
 })
-export class VehicleModule {}
+export class VehicleModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthorizationMiddleware).forRoutes(VehicleController);
+  }
+}
